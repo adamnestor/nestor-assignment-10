89550edbc4de070fc61e410aea384b6c8e93c435
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-02-26 15:42:25 -0500

    initial commit

---
+++ src/main/java/com/coderscampus/nestorassignment10/NestorAssignment10Application.java
@@ -0,0 +1,14 @@
+package com.coderscampus.nestorassignment10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestorAssignment10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestorAssignment10Application.class, args);
+	}
+	//Testing it's all set up
+
+}
---
+++ src/test/java/com/coderscampus/nestorassignment10/NestorAssignment10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.nestorassignment10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestorAssignment10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
