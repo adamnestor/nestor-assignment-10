0e78f5ec8b9cfc87c23063216f05a40d19cecd55
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-02-27 06:45:42 -0500

    Create SpoonacularAPI.java

---
+++ src/main/java/com/coderscampus/nestorassignment10/SpoonacularAPI.java
@@ -0,0 +1,12 @@
+package com.coderscampus.nestorassignment10;
+
+import org.springframework.web.client.RestTemplate;
+
+public class SpoonacularAPI {
+
+	
+	public void callSpoonacularApi() {
+		RestTemplate rt = new RestTemplate();
+	}
+	
+}
