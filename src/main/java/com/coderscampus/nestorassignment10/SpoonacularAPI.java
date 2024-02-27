93fc58931212ff7b9ef280bb6f6b638f7185546b
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-02-27 06:52:22 -0500

    Update SpoonacularAPI.java

--- src/main/java/com/coderscampus/nestorassignment10/SpoonacularAPI.java
+++ src/main/java/com/coderscampus/nestorassignment10/SpoonacularAPI.java
@@ -1,12 +1,24 @@
 package com.coderscampus.nestorassignment10;
 
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class SpoonacularAPI {
 
 	
 	public void callSpoonacularApi() {
 		RestTemplate rt = new RestTemplate();
+		
+		URI uri = UriComponentsBuilder.fromHttpUrl("https://api.spoonacular.com/mealplanner/generate")
+									 .queryParam("timeFrame", "day")
+									 .build()
+									 .toUri();
+		
+		ResponseEntity<String> response = rt.getForEntity(uri, String.class);
+		System.out.println(response);
 	}
 	
 }
